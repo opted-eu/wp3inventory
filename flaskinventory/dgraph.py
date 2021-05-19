b081c7bcb7541b323b53b6412387d49cd9e49da2
mrwunderbar666 <paul.balluff@gmail.com>
2021-05-19 18:46:49 +0200

    better datetime parsing, changed user level to numeric

--- flaskinventory/dgraph.py
+++ flaskinventory/dgraph.py
@@ -44,7 +44,8 @@ class DGraph(object):
     @staticmethod
     def parse_datetime(s):
         try:
-            return datetime.datetime.strptime(s, "%Y-%m-%dT%H:%M:%SZ")
+            # return datetime.datetime.strptime(s, "%Y-%m-%dT%H:%M:%SZ")
+            return datetime.datetime.fromisoformat(s.split('Z')[0])
         except:
             return s
 
@@ -159,6 +160,8 @@ class DGraph(object):
         
         user_data['uid'] =  '_:newuser'
         user_data['dgraph.type'] = 'User'
+        user_data['user_level'] = 0
+        user_data['user_displayname'] = user_data['email'].split('@')[0][:10]
         user_data['date_joined'] = datetime.datetime.now(datetime.timezone.utc).isoformat()
         
         txn = self.client.txn()
