cde3c3f61294c2cabd874c3718a55d3d82955256
Paul Balluff <paul.balluff@univie.ac.at>
2021-08-10 15:55:37 +0200

    subsetting fontawesome

---
+++ tools/fontawesome/subset.sh
@@ -0,0 +1,11 @@
+pyftsubset\
+ webfonts/fa-solid-900.ttf \
+ --output-file="fa-solid-subset-900.woff2" \
+ --glyphs-file="glyphs-solid.txt"
+ --flavor="woff2"
+
+pyftsubset\
+ webfonts/fa-brands-400.ttf \
+ --output-file="fa-brands-subset-400.woff2" \
+ --glyphs-file="glyphs-brands.txt"
+ --flavor="woff2"
