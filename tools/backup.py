6dd596ceab721254bcec847313bdd1d9639e9a36
Paul Balluff <paul.balluff@univie.ac.at>
2021-10-14 12:41:50 +0200

    #17 improved backup script

--- tools/backup.py
+++ tools/backup.py
@@ -1,59 +1,76 @@
 import datetime
-import os
+from pathlib import Path
+import sys
 import requests
 import json
-# first create a backup folder, e.g. /mnt/public
-# chmod +777 /mnt/public/
+import logging
+from logging.handlers import TimedRotatingFileHandler
 
-with open('tools/backup.json', 'rb') as f:
-    config = json.load(f)
+# first create a backup folder, e.g. /mnt/public/backup
 
-dgraph = "http://localhost:8080/admin"
-backup_timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M")
-backupfolder = os.path.abspath(config['backup_path'] + backup_timestamp)
+if __name__ == '__main__':
 
-query = 'mutation { export(input: {destination: "' + backupfolder + '"}) { response { message code } } }'
+    this_file = Path.resolve(__file__)
+    log_file = this_file.parent / 'backup.log'
+    config_file = this_file.parent / 'backup.json'
 
-payload = {"query": query}
+    logger = logging.getLogger()
+    formatter = logging.Formatter(
+        '%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 
-r = requests.post(dgraph, json=payload)
+    file_handler = TimedRotatingFileHandler(
+        log_file, when="midnight", interval=1, backupCount=7)
 
-if r.status_code != 200:
-    print('fail')
+    # file_handler = logging.FileHandler(LOGGING_PATH)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
-if "errors" in r.json().keys():
-    print('fail')
+    logger.info('Starting backup...')
 
-output_folder = os.path.join(backupfolder, os.listdir(backupfolder)[0])
-files = os.listdir(output_folder)
+    with open(config_file, 'rb') as f:
+        config = json.load(f)
 
-folder_token = config['ucloud_token']
-ucloud_url = config['ucloud_url']
+    dgraph = "http://localhost:8080/admin"
+    backup_timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M")
+    backupfolder = Path(config['backup_path']) / backup_timestamp
 
-upload_session = requests.Session()
-upload_session.auth = (folder_token, '')
+    query = 'mutation { export(input: {destination: "' + backupfolder + '"}) { response { message code } } }'
 
-upload_url = f'{ucloud_url}{backup_timestamp}/'
-upload_session.request('MKCOL', upload_url)
+    payload = {"query": query}
 
-for item in files:
-    item_path = os.path.join(output_folder, item)
-    with open(item_path, 'rb') as f:
-        destination_file = upload_url + item
-        upload = upload_session.put(destination_file, f)
+    r = requests.post(dgraph, json=payload)
 
-if upload.status_code != 201:
-    error_msg = f"Could not upload Backup to ucloud"
+    if r.status_code != 200:
+        logger.error(f'No response from DGraph! Status code: {r.status_code}')
+        sys.exit(1)
 
+    if "errors" in r.json().keys():
+        logger.error(r.json()['error'])
+        sys.exit(1)
 
-# except Exception as e:
-#     error_msg = f"Could not upload to GDrive! {e}"
-#     logger.error(error_msg)
-#     os.remove(OUTPUT_ARTICLES)
-#     if input_args.comments:
-#         os.remove(OUTPUT_COMMENTS)
-#     if input_args.ticker:
-#         os.remove(OUTPUT_TICKER)
-#     if input_args.comments and input_args.ticker:
-#         os.remove(OUTPUT_TICKERCOMMENTS)
-#     logjob_object.handle_error(error_msg)
+    logger.info('DGraph backup successful...')
+
+    backup_output = [child for child in backupfolder.iterdir()]
+
+    output_folder = backup_output[0]
+
+    logger.info('Uploading backup...')
+    folder_token = config['ucloud_token']
+    ucloud_url = config['ucloud_url']
+
+    upload_session = requests.Session()
+    upload_session.auth = (folder_token, '')
+
+    upload_url = f'{ucloud_url}{backup_timestamp}/'
+    upload_session.request('MKCOL', upload_url)
+
+    for item in output_folder.iterdir():
+        with open(item, 'rb') as f:
+            destination_file = upload_url + item.name
+            upload = upload_session.put(destination_file, f)
+
+    if upload.status_code != 201:
+        logger.error(f"Could not upload Backup to ucloud")
+        sys.exit(1)
+    
+    logger.info('Done!')
