b048df9aac0224a330faf774a66800ff36c5bf37
mrwunderbar666 <paul.balluff@gmail.com>
2021-12-07 20:09:37 +0100

    #44 allow dots in regex

--- flaskinventory/flaskdgraph/utils.py
+++ flaskinventory/flaskdgraph/utils.py
@@ -5,7 +5,7 @@ def strip_query(query):
     # towards certain special characters
     # for term matching and regex these characters
     # can simply be removed
-    return re.sub(r'"|/|\\|\(|\)|<|>|\{|\}|\[|\]|\$|&|#|\+|\^|\?|\*|\.', '', query)
+    return re.sub(r'"|/|\\|\(|\)|<|>|\{|\}|\[|\]|\$|&|#|\+|\^|\?|\*', '', query)
 
 def escape_query(query):
-    return re.sub(r'("|/|\\|\(|\)|<|>|\{|\}|\[|\]|\$|&|#|\+|\^|\?|\*|\.)', r'\\\1', query)
+    return re.sub(r'("|/|\\|\(|\)|<|>|\{|\}|\[|\]|\$|&|#|\+|\^|\?|\*)', r'\\\1', query)
