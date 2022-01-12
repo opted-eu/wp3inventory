eba0ca2d65a4605fe177e4b954a9bcbeed1adeed
mrwunderbar666 <paul.balluff@gmail.com>
2022-01-12 14:29:48 +0100

    #86 subunit selection now shows countries

--- flaskinventory/misc/forms.py
+++ flaskinventory/misc/forms.py
@@ -42,9 +42,9 @@ def get_subunit_choices():
     """ Helper function to get form choices 
         Queries for all subunits and returns a list of tuples
     """
-    query_string = '''{ q(func: type("Subunit"), orderasc: name)  { name uid } }'''
+    query_string = '''{ q(func: type("Subunit"), orderasc: name)  { name uid country { name } } }'''
     subunits = dgraph.query(query_string)
-    su_choices = [(subunit.get('uid'), subunit.get('name'))
+    su_choices = [(subunit.get('uid'), f"{subunit.get('name')} [{subunit['country'][0]['name'] if subunit.get('country') else 'MISSING'}]")
                   for subunit in subunits['q']]
     return su_choices
 
