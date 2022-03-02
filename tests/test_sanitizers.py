eeeb8ed1a56c26eac68579c05cc75e188b7b7684
Paul Balluff <paul.balluff@univie.ac.at>
2022-03-02 14:44:48 +0100

    added tests for edit organization

--- tests/test_sanitizers.py
+++ tests/test_sanitizers.py
@@ -8,19 +8,23 @@ if __name__ == "__main__" and __package__ is None:
     path.append(dir(path[0]))
     __package__ = "examples"
 
-from flaskinventory.users.dgraph import User
+from flask_login import current_user
+from flaskinventory.users.dgraph import User, get_user_data
 from flaskinventory.users.constants import USER_ROLES
 from flaskinventory import create_app, dgraph
 from flaskinventory.errors import InventoryValidationError, InventoryPermissionError
-from flaskinventory.main.sanitizer import OrganizationSanitizer, Sanitizer
-# from flaskinventory.flaskdgraph import NewID, UID
+from flaskinventory.main.sanitizer import Sanitizer, make_sanitizer
+from flaskinventory.main.model import Entry, Organization
 from flaskinventory.misc.forms import get_country_choices
+from flaskinventory.flaskdgraph.dgraph_types import UID
 import secrets
 import copy
 import unittest
+from datetime import datetime
 
 class Config:
     TESTING = True
+    WTF_CSRF_ENABLED = False
     SECRET_KEY = secrets.token_hex(32)
     DEBUG_MODE = False
     MAIL_SERVER = 'localhost'
@@ -53,11 +57,18 @@ class TestSanitizers(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.app = create_app(config_class=Config)
+        cls.client = cls.app.test_client()
 
         with cls.app.app_context():
             cls.derstandard_mbh_uid = dgraph.get_uid('unique_name', "derstandard_mbh")
             cls.falter_print_uid = dgraph.get_uid('unique_name', 'falter_print')
             cls.derstandard_print = dgraph.get_uid('unique_name', 'derstandard_print')
+            cls.www_derstandard_at = dgraph.get_uid('unique_name', 'www.derstandard.at')
+            cls.derstandard_facebook = dgraph.get_uid('unique_name', 'derstandard_facebook')
+            cls.derstandard_instagram = dgraph.get_uid('unique_name', 'derstandard_instagram')
+            cls.derstandard_twitter = dgraph.get_uid('unique_name', 'derstandard_twitter')
+            cls.austria_uid = dgraph.get_uid('unique_name', 'austria')
+            cls.germany_uid = dgraph.get_uid('unique_name', 'germany')
             cls.country_choices = get_country_choices()
 
     @classmethod
@@ -65,10 +76,9 @@ class TestSanitizers(unittest.TestCase):
         return super().tearDownClass()
 
     def setUp(self):
-        self.ip = '192.168.0.1'
         self.anon_user = MockUser(user_role=USER_ROLES.Anon)
-        self.contributor = MockUser()
-        self.reviewer = MockUser(user_role=USER_ROLES.Reviewer)
+        self.contributor = User(email="contributor@opted.eu")
+        self.reviewer = User(email="reviewer@opted.eu")
 
         self.mock_data1 = {
             'name': 'Test',
@@ -104,85 +114,152 @@ class TestSanitizers(unittest.TestCase):
         }
 
     def tearDown(self):
-        del(self.mock_organization)
+        with self.client:
+            self.client.get('/logout')
         
+    def test_login(self):
+        # print('-- test_login() --\n')
+
+        with self.client:
+            response = self.client.post('/login', data={'email': 'contributor@opted.eu', 'password': 'contributor123'})
+            self.assertEqual(current_user.user_displayname, 'Contributor')
+            self.client.get('/logout')
+
+        with self.client:
+            response = self.client.post('/login', data={'email': 'reviewer@opted.eu', 'password': 'reviewer123'})
+            self.assertEqual(current_user.user_displayname, 'Reviewer')
+            self.client.get('/logout')
+
+        with self.client:
+            response = self.client.post('/login', data={'email': 'wp3@opted.eu', 'password': 'admin123'})
+            self.assertEqual(current_user.user_displayname, 'Admin')
+            self.client.get('/logout')
+
 
     def test_new_entry(self):
-        
-        with self.app.app_context():
-            sanitizer = Sanitizer(self.mock_data1, self.reviewer, self.ip)
-            self.assertEqual(sanitizer.is_upsert, False)
-            self.assertCountEqual(list(sanitizer.entry.keys()), self.mock1_solution_keys)
-            self.assertEqual(type(sanitizer.entry['other_names']), set)
-            self.assertGreater(len(sanitizer.entry['other_names']), 2)
-        
-        with self.app.app_context():
-            sanitizer = Sanitizer(self.mock_data2, self.contributor, self.ip)
-            self.assertEqual(sanitizer.is_upsert, False)
-            self.assertCountEqual(list(sanitizer.entry.keys()), self.mock2_solution_keys)
-            self.assertEqual(type(sanitizer.entry['other_names']), set)
-            self.assertEqual(len(sanitizer.entry['other_names']), 2)
-        
-        with self.app.app_context():
-            self.assertRaises(InventoryPermissionError, Sanitizer, self.mock_data2, self.anon_user, self.ip)
+        # print('-- test_new_entry() --\n')
 
-        with self.app.app_context():
-            self.assertRaises(TypeError, Sanitizer, [1, 2, 3], self.contributor, self.ip)
-            self.assertRaises(TypeError, Sanitizer, self.mock_data1, 'User', self.ip)
-            self.assertRaises(TypeError, Sanitizer, self.mock_data1, self.contributor, 123)
+        with self.client:
+            response = self.client.post('/login', data={'email': 'contributor@opted.eu', 'password': 'contributor123'})
+            self.assertEqual(current_user.user_displayname, 'Contributor')
+        
+            with self.app.app_context():
+                sanitizer = Sanitizer(self.mock_data1)
+                self.assertEqual(sanitizer.is_upsert, False)
+                self.assertCountEqual(list(sanitizer.entry.keys()), self.mock1_solution_keys)
+                self.assertEqual(type(sanitizer.entry['other_names']), set)
+                self.assertGreater(len(sanitizer.entry['other_names']), 2)
+            
+                sanitizer = Sanitizer(self.mock_data2)
+                self.assertEqual(sanitizer.is_upsert, False)
+                self.assertCountEqual(list(sanitizer.entry.keys()), self.mock2_solution_keys)
+                self.assertEqual(type(sanitizer.entry['other_names']), set)
+                self.assertEqual(len(sanitizer.entry['other_names']), 2)
+            
+                self.assertRaises(TypeError, Sanitizer, [1, 2, 3])
+
+            self.client.get('/logout')
+            self.assertRaises(InventoryPermissionError, Sanitizer, self.mock_data2)
+
         
 
     def test_edit_entry(self):
+        # print('-- test_edit_entry() --\n')
         
-        edit_entry = {'entry_review_status': 'accepted', **self.mock_data1}
-        with self.app.app_context():
-            # no UID
-            self.assertRaises(InventoryValidationError, Sanitizer.edit, edit_entry, self.contributor, self.ip)
-            self.assertRaises(InventoryValidationError, Sanitizer.edit, self.mock_data2, self.contributor, self.ip)
+        with self.client:
+            response = self.client.post('/login', data={'email': 'contributor@opted.eu', 'password': 'contributor123'})
+            self.assertEqual(current_user.user_displayname, 'Contributor')
 
-        wrong_uid = {'uid': '0xfffffffff', **edit_entry}
-        with self.app.app_context():
-            # wrong uid
-            self.assertRaises(InventoryValidationError, Sanitizer.edit, wrong_uid, self.contributor, self.ip)
+            with self.app.app_context():
+                # no UID
+                edit_entry = {'entry_review_status': 'accepted', **self.mock_data1}
+                self.assertRaises(InventoryValidationError, Sanitizer.edit, edit_entry)
+                self.assertRaises(InventoryValidationError, Sanitizer.edit, self.mock_data2)
 
-        wrong_user = {'uid': self.derstandard_mbh_uid, **edit_entry}
-        with self.app.app_context():
-            # wrong permissions
-            self.assertRaises(InventoryPermissionError, Sanitizer.edit, wrong_user, self.contributor, self.ip)
+                wrong_uid = {'uid': '0xfffffffff', **edit_entry}
+                # wrong uid
+                self.assertRaises(InventoryValidationError, Sanitizer.edit, wrong_uid)
 
-        correct = {'uid': self.derstandard_mbh_uid, **edit_entry}
-        with self.app.app_context():
-            sanitizer = Sanitizer.edit(correct, self.reviewer, self.ip)
-            self.assertEqual(sanitizer.is_upsert, True)
-            self.assertNotIn('dgraph.type', sanitizer.entry.keys())
-            self.assertIn('entry_edit_history', sanitizer.entry.keys())
-            self.assertCountEqual(sanitizer.overwrite[sanitizer.entry['uid']], ['other_names', 'wikidataID'])
+                wrong_user = {'uid': self.derstandard_mbh_uid, **edit_entry}
+                # wrong permissions
+                self.assertRaises(InventoryPermissionError, Sanitizer.edit, wrong_user)
+
+            self.client.get('/logout')
+
+        with self.client:
+            response = self.client.post('/login', data={'email': 'reviewer@opted.eu', 'password': 'reviewer123'})
+            self.assertEqual(current_user.user_displayname, 'Reviewer')
+            with self.app.app_context():
+                correct = {'uid': self.derstandard_mbh_uid, **edit_entry}
+                sanitizer = Sanitizer.edit(correct)
+                self.assertEqual(sanitizer.is_upsert, True)
+                self.assertNotIn('dgraph.type', sanitizer.entry.keys())
+                self.assertIn('entry_edit_history', sanitizer.entry.keys())
+                self.assertCountEqual(sanitizer.overwrite[sanitizer.entry['uid']], ['other_names', 'wikidataID'])
 
     def test_new_org(self):
+        # print('-- test_new_org() --\n')
 
-        with self.app.app_context():
-            sanitizer = OrganizationSanitizer(self.mock_organization, self.contributor, self.ip)
-            self.assertEqual(sanitizer.is_upsert, False)
-            self.assertCountEqual(sanitizer.entry['dgraph.type'], ['Entry', 'Organization'])
-            self.assertEqual(sanitizer.entry['entry_review_status'], 'pending')
-            self.assertIsNotNone(sanitizer.set_nquads)
-            self.assertIsNone(sanitizer.delete_nquads)
-            self.assertEqual(sanitizer.entry['wikidataID'], 66048) # WikiDataID for Deutsche Bank
-            self.assertIn('employees', sanitizer.entry.keys())
-            
-        mock_org = copy.deepcopy(self.mock_organization)
-        mock_org.pop('address_string')
-        with self.app.app_context():
-            sanitizer = OrganizationSanitizer(mock_org, self.contributor, self.ip)
-            self.assertEqual(sanitizer.is_upsert, False)
-            self.assertCountEqual(sanitizer.entry['dgraph.type'], ['Entry', 'Organization'])
-            self.assertEqual(sanitizer.entry['entry_review_status'], 'pending')
-            self.assertIsNotNone(sanitizer.set_nquads)
-            self.assertIsNone(sanitizer.delete_nquads)
+        with self.client:
+            response = self.client.post('/login', data={'email': 'contributor@opted.eu', 'password': 'contributor123'})
+            self.assertEqual(current_user.user_displayname, 'Contributor')
+
+            with self.app.app_context():
+                sanitizer = make_sanitizer(self.mock_organization, Organization)
+                self.assertEqual(sanitizer.is_upsert, False)
+                self.assertCountEqual(sanitizer.entry['dgraph.type'], ['Entry', 'Organization'])
+                self.assertEqual(sanitizer.entry['entry_review_status'], 'pending')
+                self.assertIsNotNone(sanitizer.set_nquads)
+                self.assertIsNone(sanitizer.delete_nquads)
+                self.assertEqual(sanitizer.entry['wikidataID'], 66048) # WikiDataID for Deutsche Bank
+                self.assertIn('employees', sanitizer.entry.keys())
+                
+                mock_org = copy.deepcopy(self.mock_organization)
+                mock_org.pop('address_string')
+                sanitizer = make_sanitizer(self.mock_organization, Organization)
+                self.assertEqual(sanitizer.is_upsert, False)
+                self.assertCountEqual(sanitizer.entry['dgraph.type'], ['Entry', 'Organization'])
+                self.assertEqual(sanitizer.entry['entry_review_status'], 'pending')
+                self.assertIsNotNone(sanitizer.set_nquads)
+                self.assertIsNone(sanitizer.delete_nquads)
 
     def test_edit_org(self):
         overwrite_keys = ['other_names', 'wikidataID', 'country', 'owns', 'publishes']
-        pass
+        
+        mock_org_edit = {
+            "uid": self.derstandard_mbh_uid,
+            "name": "STANDARD Verlagsgesellschaft m.b.H.",
+            "country": self.austria_uid,
+            "entry_review_status": "accepted",
+            "founded": "1995-04-28T00:00:00Z",
+            "is_person": False,
+            "unique_name": "derstandard_mbh",
+            "address_string": "Vordere Zollamtsstraße 13, 1030 Wien",
+            "ownership_kind": "private ownership",
+            "publishes": [self.derstandard_print, 
+                            self.derstandard_facebook, 
+                            self.derstandard_instagram, 
+                            self.derstandard_twitter, 
+                            self.www_derstandard_at]
+        }
+        with self.client:
+            response = self.client.post('/login', data={'email': 'reviewer@opted.eu', 'password': 'reviewer123'})
+            self.assertEqual(current_user.user_displayname, 'Reviewer')
+
+            with self.app.app_context():
+                sanitizer = make_sanitizer(mock_org_edit, Organization, edit=True)
+                self.assertEqual(type(sanitizer.entry['founded']), datetime)
+                self.assertCountEqual(sanitizer.overwrite[sanitizer.entry['uid']], overwrite_keys)
+                self.assertEqual(len(sanitizer.entry['publishes']), 5)
+
+                mock_org_edit['uid'] = self.derstandard_mbh_uid
+                mock_org_edit['publishes'] = " ,".join([self.derstandard_print, self.derstandard_facebook, self.derstandard_instagram, self.derstandard_twitter])
+                mock_org_edit['country'] = self.germany_uid
+                mock_org_edit['founded'] = '2010'
+                sanitizer = make_sanitizer(mock_org_edit, Organization, edit=True)
+                self.assertEqual(len(sanitizer.entry['publishes']), 4)
+                self.assertEqual(type(sanitizer.entry['founded']), datetime)
+
 
 if __name__ == "__main__":
     unittest.main()
